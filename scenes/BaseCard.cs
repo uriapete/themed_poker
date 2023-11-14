0f99ee2c3aec2bc388e33c10e81ebf74d4a047a0
uriP <uriarteangelo@gmail.com>
2023-11-13 21:20:24 -0800

    scenes/BaseCard.cs - add exported prop Value

--- scenes/BaseCard.cs
+++ scenes/BaseCard.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class BaseCard : Area2D
 {
+    [Export] protected int Value = 0;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
