f1be26874839a6228a5a49922a20ca015303fe88
uriP <uriarteangelo@gmail.com>
2023-11-14 20:39:09 -0800

    scenes/BaseCard.cs - set text of ValueLabel to Value

--- scenes/BaseCard.cs
+++ scenes/BaseCard.cs
@@ -11,6 +11,8 @@ public partial class BaseCard : Area2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        Label valueLabel=GetNode<Label>("ValueLabel");
+        valueLabel.Text=this.Value.ToString();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
