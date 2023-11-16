10face1724c9eaed86e035197f0808c6c599e779
uriP <uriarteangelo@gmail.com>
2023-11-15 16:15:22 -0800

    scenes/BaseCard.cs - annotate and clean up code

--- scenes/BaseCard.cs
+++ scenes/BaseCard.cs
@@ -1,18 +1,26 @@
 using Godot;
-using System;
 
+// BaseCard script, meant as a basic template for displayed cards
+// this will probs be inherited
 public partial class BaseCard : Area2D
 {
-    [Export] public int Value
+    // referring to card value
+    // public set but protected set just for memory safety
+    // default: 0
+    [Export]
+    public int Value
     {
         get;
         protected set;
     } = 0;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        Label valueLabel=GetNode<Label>("ValueLabel");
-        valueLabel.Text=this.Value.ToString();
+        // on card ready, write the proper value of the card to the label
+        // remember, the label is temporary, once we confirm all the basic logic is working, we can replace the label with actual images
+        Label valueLabel = GetNode<Label>("ValueLabel");
+        valueLabel.Text = this.Value.ToString();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
