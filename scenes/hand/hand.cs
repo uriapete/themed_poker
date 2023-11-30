3ec9dd353e3533d916c10f9f4e9fe04a04505eb2
uriP <uriarteangelo@gmail.com>
2023-11-29 17:25:42 -0800

    scenes/hand/hand.cs - add var indicating when queue is being processed

--- scenes/hand/hand.cs
+++ scenes/hand/hand.cs
@@ -18,6 +18,8 @@ public partial class hand : Node2D
 
     [Signal] public delegate void CardsInQueueEventHandler();
 
+    protected bool ProcessingQueue { get; set; } = false;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
