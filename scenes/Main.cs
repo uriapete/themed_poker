8f56f57e9682ec33334ff244e232b72189afbf50
uriP <uriarteangelo@gmail.com>
2023-12-05 22:34:19 -0800

    scenes/Main.cs - remove getter CardStackPosition, as param CardStack makes this getter unneccessary

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -8,14 +8,6 @@ public partial class Main : Node2D
     [Export] public Hand HouseHandNode { get;private set; }
     [Export] public Hand PlayerHandNode { get; private set; }
 
-    public Vector2 CardStackPosition
-    {
-        get
-        {
-            Node2D cardStack = GetNode<Node2D>("CardStack");
-            return cardStack.Position;
-        }
-    }
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
