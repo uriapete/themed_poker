c4b643202e08f2c392badad9bb63e7d04df4e99a
uriP <uriarteangelo@gmail.com>
2023-12-14 21:50:36 -0800

    scenes/base_card/BaseCard.cs - write method and prop summaries

--- scenes/base_card/BaseCard.cs
+++ scenes/base_card/BaseCard.cs
@@ -5,9 +5,9 @@ using System;
 // this will probs be inherited
 public partial class BaseCard : Area2D
 {
-    // referring to card value
-    // public set but protected set just for memory safety
-    // default: 0
+    /// <summary>
+    /// The card's numerical value.
+    /// </summary>
     [Export]
     public int Value
     {
@@ -15,29 +15,52 @@ public partial class BaseCard : Area2D
         set;
     }
 
-    //how long it takes for a complete flip in secs
+    /// <summary>
+    /// How long in seconds it takes for the card to flip over.
+    /// </summary>
     [Export] public float FlipDuration { get; protected set; } = 0.2f;
 
-    //node to use for the card's front side
+    /// <summary>
+    /// The node that displays the card's value.
+    /// </summary>
     [Export] public CanvasItem ValueDisplayNode { get; protected set; }
 
+    /// <summary>
+    /// Boolean that signifies if the mouse is inside the card node area.
+    /// Changed via connected signal methods OnClickEntered/Exited.
+    /// </summary>
     protected bool MouseInside { get; set; }
 
+    /// <summary>
+    /// Signal emitted when this card is clicked on.
+    /// </summary>
+    /// <param name="card"></param>
     [Signal] public delegate void ClickEventHandler(BaseCard card);
 
     //enum for strong typing sides
     //all lower case to match animation names, which follow gdscript naming conventions
     //are signed for easy flipping
+    /// <summary>
+    /// An Enum for Sides.
+    /// Named in lower case to match animation names, which follow GDScript naming conventions.
+    /// Uses -1 and 1 for easy flipping.
+    /// </summary>
     public enum Sides
     {
         front = -1,
         back = 1,
     }
 
-    //var for: is card currently flipping?
+    /// <summary>
+    /// Boolean that signifies if card is currently flipping or not.
+    /// </summary>
     protected bool Flipping = false;
 
-    //var for easy getting and setting current side without getting node
+    /// <summary>
+    /// Easy getter and setter for the card's current side.
+    /// On get, parses the card's animatedSprite's current animation (for the card base) for the current side.
+    /// On set, converts the Side enum to a srting for the animation name.
+    /// </summary>
     public Sides CurrentSide
     {
         get
@@ -63,8 +86,11 @@ public partial class BaseCard : Area2D
         }
     }
 
-    //public fn for flipping
-    public void Flip(Sides? side)
+    /// <summary>
+    /// Public function for flipping the card.
+    /// </summary>
+    /// <param name="side"></param>
+    public void Flip(Sides side)
     {
         if (side == CurrentSide)
         {
@@ -74,7 +100,9 @@ public partial class BaseCard : Area2D
         return;
     }
 
-    //when starting flip, you MUST call this!
+    /// <summary>
+    /// Protected method to start a flip.
+    /// </summary>
     protected void BeginFlip()
     {
         //set this bool so in case to stop something from running during flip
@@ -90,8 +118,10 @@ public partial class BaseCard : Area2D
         tween.Finished += FinishFlip;
     }
 
-    //fn after first flip tween is done
-    //this fn SHOULD NOT BE CALLED BY ANYTHING OTHER THAN BEGINFLIP'S TWEEN!!!
+    /// <summary>
+    /// Protected method to finish a flip.
+    /// Should ONLY be connected to BeginFlip, DO NOT CALL.
+    /// </summary>
     protected async void FinishFlip()
     {
         //switch current side
@@ -115,40 +145,53 @@ public partial class BaseCard : Area2D
         tween.TweenProperty(this, "scale", Vector2.One, FlipDuration / 2);
 
         //after the tween is done, reset Flipping to completely finish
-        await ToSignal(tween,Tween.SignalName.Finished);
+        await ToSignal(tween, Tween.SignalName.Finished);
         Flipping = false;
     }
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
-        //it is now up to the individual node script of ValueDisplayNode to have their value render fn implemented locally
-        ValueDisplayNode.Call("RenderValue",Value);
+        //it is up to the individual node script of ValueDisplayNode to have their value render fn implemented locally
+        ValueDisplayNode.Call("RenderValue", Value);
         Flipping = false;
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    /// <summary>
+    /// Called every frame. 'delta' is the elapsed time since the previous frame.
+    /// </summary>
+    /// <param name="delta"></param>
     public override void _Process(double delta)
     {
     }
 
+    /// <summary>
+    /// Method to handle MouseInside bool when the mouse enters the area.
+    /// </summary>
     protected void OnMouseEntered()
     {
         MouseInside = true;
     }
+    /// <summary>
+    /// Method to handle MouseInside bool when the mouse exits the area.
+    /// </summary>
     protected void OnMouseExited()
     {
-        MouseInside=false;
+        MouseInside = false;
     }
 
     public override void _Input(InputEvent @event)
     {
-        if(@event is InputEventMouseButton)
+        // catches mouse inputs
+        // emits click signal when mouse is inside
+        if (@event is InputEventMouseButton)
         {
             InputEventMouseButton e = @event as InputEventMouseButton;
-            if(e.ButtonIndex == MouseButton.Left&&e.Pressed&&MouseInside)
+            if (e.ButtonIndex == MouseButton.Left && e.Pressed && MouseInside)
             {
-                EmitSignal(SignalName.Click,this);
+                EmitSignal(SignalName.Click, this);
             }
         }
     }
