b2f005a5bdb47625bd3e4d6611d3eba70b8e8a7a
uriP <uriarteangelo@gmail.com>
2024-01-11 22:11:24 -0800

    scenes/rank_labels/RankLabelParent.cs - cleanup code

--- scenes/rank_labels/RankLabelParent.cs
+++ scenes/rank_labels/RankLabelParent.cs
@@ -1,29 +1,28 @@
 using Godot;
-using System;
 
 public partial class RankLabelParent : Control
 {
     /// <summary>
     /// Label that displays House's Rank.
     /// </summary>
-    [Export]Label HouseRankLabel { get; set; }
+    [Export] Label HouseRankLabel { get; set; }
 
     /// <summary>
     /// Label that displays Player's Rank.
     /// </summary>
-    [Export]Label PlayerRankLabel { get; set; }
-    
+    [Export] Label PlayerRankLabel { get; set; }
+
     /// <summary>
     /// Offset between the end of the screen and the labels when set offscreen.
     /// </summary>
-    [Export]float OffScreenRestOffset {  get; set; }
+    [Export] float OffScreenRestOffset { get; set; }
 
     /// <summary>
     /// How long the animation of the ranks sliding on screen will last in seconds.
     /// </summary>
-    [Export(PropertyHint.Range,"0,5,or_greater")]
+    [Export(PropertyHint.Range, "0,5,or_greater")]
     float DisplayAnimationDuration { get; set; } = 0.5f;
-    
+
     /// <summary>
     /// Property that controls the visibility of both HouseRankLabel and PlayerRankLabel.
     /// </summary>
@@ -31,7 +30,7 @@ public partial class RankLabelParent : Control
     {
         get
         {
-            return HouseRankLabel.Visible&&PlayerRankLabel.Visible;
+            return HouseRankLabel.Visible && PlayerRankLabel.Visible;
         }
         set
         {
@@ -48,7 +47,7 @@ public partial class RankLabelParent : Control
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        OnScreenXPosition = (HouseRankLabel.Position.X+PlayerRankLabel.Position.X)/2;
+        OnScreenXPosition = (HouseRankLabel.Position.X + PlayerRankLabel.Position.X) / 2;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -62,8 +61,8 @@ public partial class RankLabelParent : Control
     public void MoveDisplaysOffScreen()
     {
         RankLabelsVisible = false;
-        HouseRankLabel.Position=new Vector2(Size.X+OffScreenRestOffset,HouseRankLabel.Position.Y);
-        PlayerRankLabel.Position=new Vector2(0-PlayerRankLabel.Size.X-OffScreenRestOffset,PlayerRankLabel.Position.Y);
+        HouseRankLabel.Position = new Vector2(Size.X + OffScreenRestOffset, HouseRankLabel.Position.Y);
+        PlayerRankLabel.Position = new Vector2(0 - PlayerRankLabel.Size.X - OffScreenRestOffset, PlayerRankLabel.Position.Y);
     }
 
     /// <summary>
@@ -71,16 +70,16 @@ public partial class RankLabelParent : Control
     /// </summary>
     /// <param name="HouseRank">The rank of the House's hand, as a string.</param>
     /// <param name="PlayerRank">The rank of the Player's hand, as a string.</param>
-    public void DisplayRanks(string HouseRank,string PlayerRank)
+    public void DisplayRanks(string HouseRank, string PlayerRank)
     {
         HouseRankLabel.Text = HouseRank;
         PlayerRankLabel.Text = PlayerRank;
 
-        RankLabelsVisible=true;
+        RankLabelsVisible = true;
 
         Tween tween = GetTree().CreateTween().SetParallel();
 
-        tween.TweenProperty(HouseRankLabel, "position", new Vector2(OnScreenXPosition, HouseRankLabel.Position.Y),DisplayAnimationDuration);
-        tween.TweenProperty(PlayerRankLabel, "position", new Vector2(OnScreenXPosition, PlayerRankLabel.Position.Y),DisplayAnimationDuration);
+        tween.TweenProperty(HouseRankLabel, "position", new Vector2(OnScreenXPosition, HouseRankLabel.Position.Y), DisplayAnimationDuration);
+        tween.TweenProperty(PlayerRankLabel, "position", new Vector2(OnScreenXPosition, PlayerRankLabel.Position.Y), DisplayAnimationDuration);
     }
 }
