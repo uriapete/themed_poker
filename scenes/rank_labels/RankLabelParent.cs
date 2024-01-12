b2eedb759352764b66aae4f0c729a4d78f6827d4
uriP <uriarteangelo@gmail.com>
2024-01-11 16:01:32 -0800

    scenes/rank_labels - make and set new export vars for rank labels

--- scenes/rank_labels/RankLabelParent.cs
+++ scenes/rank_labels/RankLabelParent.cs
@@ -3,6 +3,16 @@ using System;
 
 public partial class RankLabelParent : Control
 {
+    /// <summary>
+    /// Label that displays House's Rank.
+    /// </summary>
+    [Export]Label HouseRankLabel { get; set; }
+
+    /// <summary>
+    /// Label that displays Player's Rank.
+    /// </summary>
+    [Export]Label PlayerRankLabel { get; set; }
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
