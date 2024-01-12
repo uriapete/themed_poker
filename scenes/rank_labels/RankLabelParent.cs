69d01590bfce6eea2c8d71b4ca78cb09e32e7857
uriP <uriarteangelo@gmail.com>
2024-01-11 16:05:18 -0800

    scenes/rank_labels/RankLabelParent.cs - new control prop for visibility of both Rank Labels

--- scenes/rank_labels/RankLabelParent.cs
+++ scenes/rank_labels/RankLabelParent.cs
@@ -13,6 +13,22 @@ public partial class RankLabelParent : Control
     /// </summary>
     [Export]Label PlayerRankLabel { get; set; }
     
+    /// <summary>
+    /// Property that controls the visibility of both HouseRankLabel and PlayerRankLabel.
+    /// </summary>
+    public bool RankLabelsVisible
+    {
+        get
+        {
+            return HouseRankLabel.Visible&&PlayerRankLabel.Visible;
+        }
+        set
+        {
+            HouseRankLabel.Visible = value;
+            PlayerRankLabel.Visible = value;
+        }
+    }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
