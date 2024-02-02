96eafef6cb78577d61d8afc47c18d2772c505309
uriP <uriarteangelo@gmail.com>
2024-02-01 22:49:44 -0800

    scenes/globals/global/Global.cs - new static var TreePath, set on _Ready

--- scenes/globals/global/Global.cs
+++ scenes/globals/global/Global.cs
@@ -3,9 +3,15 @@ using System;
 
 public partial class Global : Node
 {
+    /// <summary>
+    /// Tree Path to the Global Autoload.
+    /// </summary>
+    public static string TreePath { get; private set; }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        TreePath = GetPath();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
