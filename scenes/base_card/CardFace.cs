ff33fea400662042b4f59a1544919e86842fc435
uriP <uriarteangelo@gmail.com>
2024-03-15 19:04:55 -0700

    scenes/base_card/CardFace.cs - change export var array Labels to dictionary ValueFaceNames

--- scenes/base_card/CardFace.cs
+++ scenes/base_card/CardFace.cs
@@ -4,7 +4,7 @@ using System;
 public partial class CardFace : Node2D
 {
     [Export]public AnimatedSprite2D FaceSprite { get;private set; }
-    [Export]public Godot.Collections.Array<ValueLabel> Labels { get; private set; }
+    [Export]public Godot.Collections.Dictionary<int,string> ValueFaceNames { get; private set; }
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
