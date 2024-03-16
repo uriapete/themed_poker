b54f8057c18f17e950cb5f4693f739eb74dfb22f
uriP <uriarteangelo@gmail.com>
2024-03-15 19:09:36 -0700

    scenes/base_card/CardFace.cs - export categories

--- scenes/base_card/CardFace.cs
+++ scenes/base_card/CardFace.cs
@@ -3,9 +3,12 @@ using System;
 
 public partial class CardFace : Node2D
 {
+    [ExportCategory("Face Sprite")]
     [Export]public AnimatedSprite2D FaceSprite { get;private set; }
     [Export]public Godot.Collections.Dictionary<int,string> ValueFaceNames { get; private set; }
+    [ExportCategory("Value Labels")]
     [Export]public Godot.Collections.Array<Label> Labels { get; private set; }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
